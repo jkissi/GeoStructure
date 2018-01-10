7d318bb5618d9304a74bbedbf297de840bdbfb32
kissi <kissiameyaw@gmail.com>
2018-01-10 17:43:42 +0000

    Worked only on the calculation. Added flowerboxing and comments to the files. Added terminating disp() statement to files where necessary. Added filesep to files where required. Modified gitignore this commit. Cheers

--- GeoStructure/calculation/calculation__get_mahal_dist.m
+++ GeoStructure/calculation/calculation__get_mahal_dist.m
@@ -1,6 +1,14 @@
-function d = calculation__get_mahal_dist(A, B) 
-% Return mahalanobis distance of two data matrices 
-% A and B (row = object, column = feature)
+% -------------------------------------------------------------------------
+% calculation__get_mahal_dist function
+% -------------------------------------------------------------------------
+function [ d ] = calculation__get_mahal_dist(A, B) 
+%% ------------------------------------------------------------------------
+% Discussion
+% -------------------------------------------------------------------------
+% Function to calculate the Mahalanobis distance between two points 
+% Return mahalanobis distance of two data matrices A and B
+% -------------------------------------------------------------------------
+
 [ n1, k1 ] = size(A);
 [ n2, k2 ] = size(B);
 n = n1 + n2;
@@ -13,4 +21,10 @@ else
     pC = n1/n*cA + n2/n*cB; % pooled covariance matrix
     d = sqrt(xDiff*inv(pC)*xDiff'); % mahal distance calculation
     
+end
+
+disp('Execution complete. Function calculation__get_mahal_dist.m terminating.');
+% -------------------------------------------------------------------------
+% Terminate 
+% -------------------------------------------------------------------------
 end 
