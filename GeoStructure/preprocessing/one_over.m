ff836386060e7f810c08c82549091408de3535a7
cpsx <jkissiam@uwo.ca>
2019-08-01 20:56:06 -0400

    Minor change to one_over program

--- GeoStructure/preprocessing/one_over.m
+++ GeoStructure/preprocessing/one_over.m
@@ -27,7 +27,7 @@ files = {files.name}';                      % file names
 %output_folder = sprintf('Frames from %s', folder, input_file_name);
 %[pathstr,name] = fileparts(path_input);
 %c_array = strsplit(path_input,'\')
-output_folder = [absolute_output_path, output_folder];
+output_folder = [absolute_output_path, '\', output_folder];
 % Create the folder if it doesn't exist already.
 if ~exist(output_folder, 'dir')
     mkdir(output_folder);
@@ -39,12 +39,12 @@ for i = 1:numel(files)
     if(mod(i, divisor) == 0)
         fname = fullfile(path_input, files{i});     % full path to file
         copyfile(fname, output_folder);
-        disp([i, 'th file(', fname, ') copied and moved to ', output_folder])
+        disp([int2str(i), 'th file(', fname, ') copied and moved to ', output_folder])
     end
 end
 
 
-disp(['All files copied and moved to ', output_folder, '. \n']);
+disp(['All files copied and moved to ', output_folder, '.']);
 
 %--------------------------------------------------------------------------
 % Terminate
